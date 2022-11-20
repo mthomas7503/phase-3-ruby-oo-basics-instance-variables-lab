34690db7b976fb5672559078bc6eef46ece62841
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-11-20 15:40:21 -0500

    commit tree 2ee82eb6208ebb9ac606a62b919c794036bbffaf

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
 
+    def name
+        @this_dogs_name
+    end
+
+    def breed=(dogs_breed)
+        @breed = dogs_breed
+    end
+
+    def breed
+        @breed
+    end
+end
+
+
+class Person
+    attr_accessor :name
+    attr_accessor :job
+
+    def initialize(name, job)
+        @name = name
+        @job = job
+    end
 end
